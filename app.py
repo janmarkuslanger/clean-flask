6e4207d796f0e7ffe8c29ad74743b4e1fe4a0c7d
Jan-Markus Langer <hallo@janmarkuslanger.de>
2019-10-14 21:15:17 +0200

    templating

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.secret_key = 'My Key'
