b73f455b114b0cc48493c99a3476bf4ae7b6ecbc
Jan-Markus Langer <hallo@janmarkuslanger.de>
2019-10-14 21:07:37 +0200

    JS / SCSS

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: ['./theme/scss/index.scss', './theme/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, './static/'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              path: path.resolve(__dirname, './static/'),
+              name: 'bundle.css',
+            },
+          },
+          {loader: 'extract-loader'},
+          {loader: 'css-loader'},
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                includePaths: ['./node_modules'],
+              }
+            },
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+        },
+      }
+    ],
+  },
+};
