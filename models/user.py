6717d05ca0a4084caab53d4f73d0c1314ead4c2d
Jan-Markus Langer <hallo@janmarkuslanger.de>
2019-10-27 15:52:31 +0100

    Add user model

---
+++ models/user.py
@@ -0,0 +1,12 @@
+!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    username = Column(String, unique=True)
